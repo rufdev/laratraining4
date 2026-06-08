c980152574a51980174b0e6e589398ecfa924e72
RUF.DEV <aguilarufino@gmail.com>
2026-06-08 16:27:37 +0800

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\CategoryController;
+
+ Route::get('/', function () {
+    return Inertia::render('Welcome');
+ })->name('home');
+
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('categories', [CategoryController::class, 'index'])->name('categories');
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
