2d211f4fefaa3b2356bca2ce9ac041d00431dd84
RUF.DEV <aguilarufino@gmail.com>
2026-06-11 10:48:55 +0800

    added Create, Edit and Delete UI for Category

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,7 +1,7 @@
 import type { ClassValue } from "clsx"
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { Updater } from '@tanstack/vue-table'
+import { Updater } from '@tanstack/vue-table';
 import { Ref } from 'vue';
 
 export function cn(...inputs: ClassValue[]) {
